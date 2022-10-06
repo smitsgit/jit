3b1104bd4b5174d29e357c5c200300aa9d4ad43d
Smital Desai <desaismital@gmail.com>
2022-10-06 18:01:27 +0530

    Add support for commit command just listing the files

--- jit/main.py
+++ jit/main.py
@@ -10,29 +10,49 @@ def callback():
     """
 
 class Repository:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, name = None):
+        if name:
+            self.name = name
         path = Path(".")
-        repo_path = (path / f"{name}")
-        self.gitdir = (repo_path / ".git")
-        self.gitdir.mkdir(parents=True)
+        self.work_dir = (path / f"{name}") if name else (path)
+        self.gitdir = (self.work_dir / ".git")
+
 
     def create_git_dir_structure(self):
+        self.gitdir.mkdir(parents=True)
+
         for dir in ["branches", "objects", "refs//tags", "refs//heads"]:
-            (self.gitdir/ dir).mkdir(parents=True)
+            try:
+                (self.gitdir/ dir).mkdir(parents=True)
+            except PermissionError as e:
+                print(f"Permission denied {dir}")
 
         for file in ["description", "HEAD"]:
-            (self.gitdir / file).touch()
-
+            try:
+                (self.gitdir / file).touch()
+            except PermissionError as e:
+                print(f"Permission denied {file}")
 
 @app.command()
-def init(name: str):
+def init(name: str = "."):
     """
     Initialize the empty repository
     """
-    typer.echo("Cooking git for ya ...")
     repo = Repository(name)
     repo.create_git_dir_structure()
+    print(f"Initialized empty repository in {repo.gitdir.absolute()}")
+
+
+@app.command()
+def commit():
+    """
+    list the files to commit - [ for the time being ]
+    :return:
+    """
+    repo = Repository()
+    files = [item.name for item in repo.work_dir.iterdir() if Path.is_file(item)]
+    print(str(files))
+
 
 
 
