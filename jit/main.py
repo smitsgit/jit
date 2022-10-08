11a61448bee12d6c1ef05d80be2d2c5ff514fb2d
Smital Desai <desaismital@gmail.com>
2022-10-08 17:04:34 +0530

    Add support for storing tree objects

--- jit/main.py
+++ jit/main.py
@@ -3,6 +3,7 @@ import zlib
 
 import typer
 from pathlib import Path
+from dataclasses import dataclass
 
 app = typer.Typer()
 
@@ -48,6 +49,22 @@ def init(name: str = "."):
     print(f"Initialized empty repository in {repo.gitdir.absolute()}")
 
 
+class Tree:
+    def __init__(self, entries):
+        self.entries = entries
+
+    def type(self):
+        return b"tree"
+
+    def serialize(self):
+        mode = b'100644'
+        content = b''
+        for item in self.entries:
+            content += mode + b' ' + item.name.encode() + b'\x00' + bytes.fromhex(item.sha)
+        return content
+
+
+
 @app.command()
 def commit():
     """
@@ -58,11 +75,26 @@ def commit():
     files = [item for item in repo.work_dir.iterdir() if Path.is_file(item)]
     db_path = (repo.gitdir / "objects")
     database = Database(db_path)
+    entries = []
 
     for item in files:
-        file_content = item.read_bytes()
-        blob = Blob(file_content)
-        database.store_object(blob)
+        with open(item, "rb") as fd:
+            file_content = fd.read()
+            blob = Blob(file_content)
+            sha = database.store_object(blob)
+            entries.append(Entry(item.name, sha))
+
+    # Tree is a snapshot of all the entries, so it should be made
+    # out of the contents of all the entries which participated in that
+    # commit
+    tree = Tree(entries)
+    database.store_object(tree)
+
+
+@dataclass()
+class Entry:
+    name: str
+    sha: str
 
 
 class Blob:
@@ -81,16 +113,17 @@ class Blob:
     def type(self):
         return b'blob'
 
+    def serialize(self):
+        return self.data
+
 
 class Database:
     def __init__(self, db_path: Path):
         self.path = db_path
 
-    def store_object(self, obj: Blob):
+    def store_object(self, obj):
         content_to_write = self.get_content_bytes(obj)
         sha = self.hash_it(content_to_write, obj)
-        print(sha)
-
         # compute the path
         path = (self.path / sha[:2])
         path.mkdir(parents=True, exist_ok=True)
@@ -99,6 +132,7 @@ class Database:
         path = (path / sha[2:])
         with open(path, "wb") as file:
             file.write(zlib.compress(content_to_write))
+        return sha
 
     def hash_it(self, content_to_write, obj):
         sha = hashlib.sha1(content_to_write).hexdigest()
@@ -106,6 +140,6 @@ class Database:
         return sha
 
     def get_content_bytes(self, obj):
-        blob_content = obj.data
-        content_to_write = obj.type() + b' ' + str(len(blob_content)).encode() + b'\x00' + blob_content
+        obj_content = obj.serialize()
+        content_to_write = obj.type() + b' ' + str(len(obj_content)).encode() + b'\x00' + obj_content
         return content_to_write
